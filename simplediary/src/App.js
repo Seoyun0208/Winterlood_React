b6a8e2c4e702bf994d8cf493a5146d67172bf41b
Seoyun0208 <sylph0105@naver.com>
2023-02-04 11:23:09 +0900

    [FEAT] 6-8. React 에서 API 호출하기

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,14 +1,35 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import './App.scss';
 import DiaryEditor from './components/DiaryEditor';
 import DiaryList from './components/DiaryList';
 // import Lifecycle from './components/Lifecycle/Lifecycle';
-import Lifecycle2 from './components/Lifecycle/Lifecycle2';
+// import Lifecycle2 from './components/Lifecycle/Lifecycle2';
+
+
+// https://jsonplaceholder.typicode.com/comments
 
 function App() {
 
   const [data, setData] = useState([]);
   const dataId = useRef(0);
+
+  const getData = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/comments').then(res => res.json());
+    const initData = res.slice(0, 20).map(item => {
+      return {
+        id: dataId.current++,
+        author: item.email,
+        content: item.body,
+        emotion: Math.floor((Math.random() * 5) + 1),
+        created_date: new Date().getTime()
+      }
+    });
+    setData(initData);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -33,7 +54,7 @@ function App() {
   return (
     <div className="App">
       {/* <Lifecycle /> */}
-      <Lifecycle2 />
+      {/* <Lifecycle2 /> */}
       <DiaryEditor onCreate={onCreate} />
       <DiaryList diaryList={data} onRemove={onRemove} onSave={onSave} />
     </div>
